0d670502045c903181fe5ccb43b7580d7d992b8b
Thomas Unterthiner <unterthiner@bioinf.jku.at>
2015-11-24 13:05:56 +0100

    Import train function by default

--- binet/__init__.py
+++ binet/__init__.py
@@ -3,7 +3,7 @@ from binet import op
 from binet import util
 from binet.neuralnet import NeuralNet
 from binet.datasets import load_dataset
-from binet.util import plot_images, print_system_information
+from binet.util import train, plot_images, print_system_information
 
 
 import pkg_resources as __pkg_resources
